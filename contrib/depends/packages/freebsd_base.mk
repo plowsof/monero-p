5bab3611dd0bb6699def012f7f775379e69afb51
plowsof <77655812+plowsof@users.noreply.github.com>
2024-10-14 08:15:05 +0100

    Update freebsd_base.mk

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -3,7 +3,7 @@ $(package)_version=14.1
 $(package)_download_path=https://download.freebsd.org/ftp/releases/amd64/$($(package)_version)-RELEASE/
 $(package)_download_file=base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
-$(package)_sha256_hash=4599023ac136325b86f2fddeec64c1624daa83657e40b00b2ef944c81463a4ff
+$(package)_sha256_hash=bb451694e8435e646b5ff7ddc5e94d5c6c9649f125837a34b2a2dd419732f347
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
